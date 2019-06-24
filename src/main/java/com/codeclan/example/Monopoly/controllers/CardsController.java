fedb0a4bdbd5beccd8f50853cdd2a4c4200e2a3d
lindsey <lj0978@hotmail.co.uk>
2019-06-24 13:55:29 +0100

    whatgit add .git add .

--- src/main/java/com/codeclan/example/Monopoly/controllers/CardsController.java
+++ src/main/java/com/codeclan/example/Monopoly/controllers/CardsController.java
@@ -1,9 +1,14 @@
 package com.codeclan.example.Monopoly.controllers;
 
+import com.codeclan.example.Monopoly.models.Cards;
+import com.codeclan.example.Monopoly.models.Squares;
 import com.codeclan.example.Monopoly.repositories.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/cards")
@@ -11,4 +16,10 @@ public class CardsController {
 
     @Autowired
     CardsRepository cardsRepository;
+
+    @GetMapping(value = "")
+    public List<Cards> getCards(){
+        return cardsRepository.findAll();
 }
+}
+
